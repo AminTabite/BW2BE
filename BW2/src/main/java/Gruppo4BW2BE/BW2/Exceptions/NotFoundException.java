23db1fd32520af060c81a46e435519e84e30d099
BottiniChristian <ciccioboto@icloud.com>
2025-10-27 15:10:09 +0100

    aggiunto un paio di commenti e modificato fattureService e aggiunto una cosa a NotFoundException

--- BW2/src/main/java/Gruppo4BW2BE/BW2/Exceptions/NotFoundException.java
+++ BW2/src/main/java/Gruppo4BW2BE/BW2/Exceptions/NotFoundException.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message, UUID id) {
-        super(message);
+        super(message);}
+
+    public NotFoundException(String message) {
     }
 }
