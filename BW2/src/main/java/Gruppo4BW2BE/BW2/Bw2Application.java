17dc9f77769833fe5fb22a2aa73d833a96da5b51
AminTabite <Aminepicode13@gmail.com>
2025-10-27 09:45:17 +0100

    Start project

---
+++ BW2/src/main/java/Gruppo4BW2BE/BW2/Bw2Application.java
@@ -0,0 +1,13 @@
+package Gruppo4BW2BE.BW2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bw2Application.class, args);
+	}
+
+}
