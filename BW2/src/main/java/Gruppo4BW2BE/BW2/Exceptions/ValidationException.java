060aa6ea196c80b9ed273e89f2020699c5028cc0
BottiniChristian <ciccioboto@icloud.com>
2025-10-27 12:25:19 +0100

    fattura

--- BW2/src/main/java/Gruppo4BW2BE/BW2/Exceptions/ValidationException.java
+++ BW2/src/main/java/Gruppo4BW2BE/BW2/Exceptions/ValidationException.java
@@ -1,7 +1,9 @@
 package Gruppo4BW2BE.BW2.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
+    public ValidationException(String message) { super(message); }
 }
