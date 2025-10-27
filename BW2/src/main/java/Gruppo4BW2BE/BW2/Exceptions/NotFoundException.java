e181e52454fab853837c3ed8388942ca47f356d6
giacomop1196 <giacomo.pillitteri65@gmail.com>
2025-10-27 12:23:50 +0100

    commit

--- BW2/src/main/java/Gruppo4BW2BE/BW2/Exceptions/NotFoundException.java
+++ BW2/src/main/java/Gruppo4BW2BE/BW2/Exceptions/NotFoundException.java
@@ -1,7 +1,9 @@
 package Gruppo4BW2BE.BW2.Exceptions;
 
+import java.util.UUID;
+
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
+    public NotFoundException(String message, UUID id) {
         super(message);
     }
 }
--- BW2/src/main/java/Gruppo4BW2BE/BW2/Repositories/ClienteRepository.java
+++ BW2/src/main/java/Gruppo4BW2BE/BW2/Repositories/ClienteRepository.java
@@ -4,7 +4,13 @@ import Gruppo4BW2BE.BW2.Entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+
+    Optional<Cliente> findByPartitaIva(String partitaIva);
+
+    Optional<Cliente> findByEmail(String email);
+
 }
